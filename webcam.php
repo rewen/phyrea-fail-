b5f15dcc7eb9d0583bd31e64ab87452564c8bc16
Kamil <kzakoscielny3@hotmail.com>
2018-09-19 21:19:00 +0100

    Create webcam.php

---
+++ webcam.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" herf="css/hujowevideo.css">
+    </head>
+
+        <body>
+             <div class="lol">
+                <video id="video" with="400" hight="300" autoplay></video>
+             </div>
+
+                <script src="video.js"></script>
+        </body>
+</html>
